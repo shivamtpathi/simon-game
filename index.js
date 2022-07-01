d9ecf4b68d90390c787d3f5b8a6c2d8fc4aa7bc9
Shivam Tripathi <103302063+shivamtpathi@users.noreply.github.com>
2022-07-01 12:19:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).on("tap",function(){
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
+
 
 
 $(document).keypress(function() {
